ed64fe3f9cb078b6a546de552f0393215c8c0eba
Sam Reh <samuelreh@gmail.com>
2015-10-01 15:07:36 -0700

    Drop ask me and signup reason fields in favor of custom

--- db/migrate/20151001204003_drop_listing_id_from_custom_field_value.rb
+++ db/migrate/20151001204003_drop_listing_id_from_custom_field_value.rb
@@ -1,5 +1,7 @@
 class DropListingIdFromCustomFieldValue < ActiveRecord::Migration
   def change
     remove_column :custom_field_values, :listing_id
+    remove_column :people, :ask_me
+    remove_column :people, :signup_reason
   end
 end
