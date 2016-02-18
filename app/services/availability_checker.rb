70e297dc892c4adc7138e5a82f7de95c0233b9c4
Sam Reh <samuelreh@gmail.com>
2016-02-18 13:15:38 -0800

    Change to send reminders next week

--- app/services/availability_checker.rb
+++ app/services/availability_checker.rb
@@ -1,7 +1,7 @@
 class AvailabilityChecker
 
   def self.call
-    return unless Date.today.tuesday? && (Date.today.cweek % 2 == 1)
+    return unless Date.today.tuesday? && (Date.today.cweek % 2 == 0)
 
     Community.all.each do |community|
       community.listings.non_badge.currently_open.each do |listing|
